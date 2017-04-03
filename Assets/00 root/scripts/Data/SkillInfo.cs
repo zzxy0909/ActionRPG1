acbd2dfe0024f9b1f4d279cd536fbb612436638d
zzxy0909 <zzxy0909@naver.com>
2017-04-03 18:10:33 +0900

    Game Data 설정 관련 중 data manager 정리 필요,

---
+++ Assets/00 root/scripts/Data/SkillInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SkillInfo
+{
+    public int m_id;// in table.
+    public float m_coolTime = 0;
+    public string m_aniName;
+    public float m_attackRange = 0;
+    public bool m_canCalcel = false;
+    public int m_targetType = 0; // 0 : attack enemy, 1: our team assistance
+    public float m_lastUseTime = 0;
+
+    public float m_hpLimit = 0;
+    public int m_useChance = 0;
+
+    public enum eSkillType
+    {
+        NomalAttack,
+        Skill
+    }
+    public eSkillType m_SkillType = eSkillType.NomalAttack;
+
+
+}
---
+++ Assets/00 root/scripts/InGame/Skill_controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill_controller : MonoBehaviour {
+    public List<SkillInfo> m_listSkill = new List<SkillInfo>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetupSkill()
+    {
+
+    }
+}
